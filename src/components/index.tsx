ae5c2af4810abb1462595ea2c0b2e2ccc1040ce0
vadim <vadim96dk@gmail.com>
2023-03-01 17:34:33 +0200

    remove unused component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,10 +1,9 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Input from "./Input/Input";
-import CardCreateForm from "./CardCreateForm/CardCreateForm";
 import Button from "./Button/Button";
 import Textarea from "./Textarea/Textarea";
 import Card from "./Card/Card";
 import Loader from "./Loader/Loader";
 
-export { Header, Footer, Input, CardCreateForm, Button, Textarea, Card, Loader }
+export { Header, Footer, Input, Button, Textarea, Card, Loader }
